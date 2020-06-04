c4b0a7ed3ae708c8fd30bc1e0d7829228a15e3a3
Hisni <hisnionline@gmail.com>
2020-06-04 23:16:33 +0530

    Login Added

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+// import Input from '../../components/UI/Input/Input';
+// import Button from '../../components/UI/Button/Button';
+// import Spinner from '../../components/UI/Spinner/smallSpinner';
+import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import { updateObject, checkValidity } from '../../shared/utility';
+import Aux from '../../hoc/Auxiliary/Auxiliary'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { SocialMediaIconsReact } from 'social-media-icons-react';
+
+class Login extends Component {
+    state = {
+        controls: {
+            Email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            Password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        },
+        isSignup: false,
+        formIsValid: false
+    }
+
+    componentDidMount() {
+        if ( this.props.error ) {
+            this.props.setAuth();
+        }
+    }
+
+    inputChangedHandler = ( event, controlName ) => {
+        const updatedControls = updateObject( this.state.controls, {
+            [controlName]: updateObject( this.state.controls[controlName], {
+                value: event.target.value,
+                valid: checkValidity( event.target.value, this.state.controls[controlName].validation ),
+                touched: true
+            } )
+        } );
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedControls) {
+            formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
+        }
+        
+        this.setState( { controls: updatedControls, formIsValid: formIsValid } );
+    }
+
+    submitHandler = ( event ) => {
+        event.preventDefault();
+        this.props.onAuth( this.state.controls.Email.value, this.state.controls.Password.value );
+    }
+
+    render () {
+        const formElementsArray = [];
+        for ( let key in this.state.controls ) {
+            formElementsArray.push( {
+                id: key,
+                config: this.state.controls[key]
+            } );
+        }
+
+        let form = (
+                <Aux>
+                    <div className="container">
+                        <div id="login">
+                            <form className="login-form" onSubmit={this.submitHandler}>
+                            <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                            <input
+                                autoFocus
+                                maxLength="25"
+                                onChange={( event ) => this.inputChangedHandler( event, "Email" )}
+                                placeholder="Email"
+                                type="email"
+                                value={this.state.controls.Email.value}
+                                required
+                            />
+                            <span><FontAwesomeIcon icon={faLock} /></span>
+                            <input
+                                autoComplete="off"
+                                maxLength="16"
+                                onChange={( event ) => this.inputChangedHandler( event, "Password" )}
+                                placeholder="Password"
+                                type="password"
+                                value={this.state.controls.Password.value}
+                                required
+                            />
+                            <input type="submit" value="Log in"/>
+                            </form>
+                        </div>
+                    </div>
+                    
+                    <div className="sign-up__actions clearfix">
+                        <p>Not a member? <a href="/register">Sign up now</a><span><FontAwesomeIcon icon={faArrowRight}/></span></p>
+                    </div>
+
+                    <div className="sign-in__actions">
+                        <ul>
+                            <li><SocialMediaIconsReact icon="github" iconColor="#2b161e" backgroundColor="#929292" iconSize="8"/></li>
+                            <li><SocialMediaIconsReact icon="googleplus" iconColor="#2b161e" backgroundColor="#929292" iconSize="8" /></li>
+                            <li><SocialMediaIconsReact icon="facebook" iconColor="#2b161e" backgroundColor="#929292" iconSize="8"/></li>
+                            <li><SocialMediaIconsReact icon="twitter" iconColor="#2b161e" backgroundColor="#929292" iconSize="8"/></li>
+                        </ul>
+                    </div> 
+                </Aux>);
+
+        let loadSpinner = null;
+
+        if (this.props.loading) {
+            // loadSpinner = <Spinner />
+        }
+
+        let errorMessage = null;
+
+        if (this.props.error) {
+            errorMessage = (
+                <p>Invalid email or password. Please try again.</p>
+            );
+        }
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to={'/profile'}/>
+        }
+
+        return (
+            <div className={classes.Page} >
+                <div className={classes.Auth}>
+                    {authRedirect}
+                    {form}
+                    <div className={classes.Extras}>
+                        {loadSpinner}
+                        {errorMessage}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: ( email, password ) => dispatch( actions.authSignIn( email, password ) ),
+        setAuth: () => dispatch(actions.setAuth()),
+    };
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( Login );
