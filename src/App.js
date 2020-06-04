f099f17a6d7b7273d38db294f1659f5e3cbb5415
Hisni <hisnionline@gmail.com>
2020-06-04 23:15:21 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { IconContext } from "react-icons";
+
+// import './App.css';
+import './styles/core.scss';
+
+import Main from './containers/Main/Main';
+
+class App extends Component {
+
+    render() {
+        return (
+            <IconContext.Provider value={{ color: "rgb(3, 78, 41)",style: { verticalAlign: 'middle' } , className: "global-class-name" }}>
+                <div className="App">
+                    <Main />
+                </div>
+            </IconContext.Provider>
+        );
+    }
+}
+
+export default App;
+
