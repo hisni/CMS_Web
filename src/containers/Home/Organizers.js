3fdf72aeb135c95cd826c6ff14f575c1b6c3a7ff
Hisni <hisnionline@gmail.com>
2020-08-22 00:19:23 +0530

    Updates

--- src/containers/Home/Organizers.js
+++ src/containers/Home/Organizers.js
@@ -1,175 +1,74 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import React, {Component} from 'react';
+import {storage} from "../../firebase/firebase";
+import axios from 'axios';
 
-const useStyles1 = makeStyles(theme => ({
-	root: {
-		flexShrink: 0,
-		color: theme.palette.text.secondary,
-		marginLeft: theme.spacing(2.5),
-	},
-}));
+class Organizers extends Component {
 
-function TablePaginationActions(props) {
-	const classes = useStyles1();
-	const theme = useTheme();
-	const { count, page, rowsPerPage, onChangePage } = props;
-
-	function handleFirstPageButtonClick(event) {
-		onChangePage(event, 0);
+	state = {
+		imageAsFile:null,
+		imageAsUrl:null,
 	}
 
-	function handleBackButtonClick(event) {
-		onChangePage(event, page - 1);
+	componentDidMount() {
+
+	}
+	
+	inputChangedHandler = (event) =>{
+		const image = event.target.files[0]
+		this.setState({imageAsFile: image})
 	}
 
-	function handleNextButtonClick(event) {
-		onChangePage(event, page + 1);
-	}
+	onSumbitHandler = (event) => {
+        event.preventDefault();
+		
+		if(this.state.imageAsFile === null ) {
+			console.error(`not an image, the image file is a ${typeof(this.state.imageAsFile)}`);
+		}
+        
+		const uploadTask = storage.ref(`/Speakers/${this.state.imageAsFile.name}`).put(this.state.imageAsFile);
+		
+		uploadTask.on('state_changed', 
+		(snapShot) => {
+			//takes a snap shot of the process as it is happening
+			console.log(snapShot)
+		}, (err) => {
+			//catches the errors
+			console.log(err)
+		}, () => {
+			// gets the functions from storage refences the image storage in firebase by the children
+			// gets the download url then sets the image from firebase as the value for the imgUrl key:
+			storage.ref('/Speakers').child(this.state.imageAsFile.name).getDownloadURL()
+			.then(fireBaseUrl => {
+				this.setState({imageAsUrl: fireBaseUrl});
+				console.log(fireBaseUrl);
+			})
+		})
 
-	function handleLastPageButtonClick(event) {
-		onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-	}
+    }
 
-	return (
-		<div className={classes.root}>
-		<IconButton
-			onClick={handleFirstPageButtonClick}
-			disabled={page === 0}
-			aria-label="first page"
-		>
-			{theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-		</IconButton>
-		<IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-			{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-		</IconButton>
-		<IconButton
-			onClick={handleNextButtonClick}
-			disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-			aria-label="next page"
-		>
-			{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-		</IconButton>
-		<IconButton
-			onClick={handleLastPageButtonClick}
-			disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-			aria-label="last page"
-		>
-			{theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-		</IconButton>
-		</div>
-	);
+	
+
+    render() {
+
+		let upimage = null;
+
+        if (this.state.imageAsUrl) {
+            upimage = <img src={this.state.imageAsUrl} alt="asdad" />
+		}
+		
+        return (
+			<div className="App">
+				<form onSubmit={this.onSumbitHandler}>
+					<input 
+						type="file"
+						onChange={this.inputChangedHandler}
+					/>
+					<button>Upload</button>
+				</form>
+				{upimage}
+			</div>
+        );
+    }
 }
 
-TablePaginationActions.propTypes = {
-	count: PropTypes.number.isRequired,
-	onChangePage: PropTypes.func.isRequired,
-	page: PropTypes.number.isRequired,
-	rowsPerPage: PropTypes.number.isRequired,
-};
-
-function createData(name, calories, fat) {
-	return { name, calories, fat };
-}
-
-const rows = [
-	createData('Cupcake', 305, 3.7),
-	createData('Donut', 452, 25.0),
-	createData('Eclair', 262, 16.0),
-	createData('Frozen yoghurt', 159, 6.0),
-	createData('Gingerbread', 356, 16.0),
-	createData('Honeycomb', 408, 3.2),
-	createData('Ice cream sandwich', 237, 9.0),
-	createData('Jelly Bean', 375, 0.0),
-	createData('KitKat', 518, 26.0),
-	createData('Lollipop', 392, 0.2),
-	createData('Marshmallow', 318, 0),
-	createData('Nougat', 360, 19.0),
-	createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
-const useStyles2 = makeStyles(theme => ({
-	root: {
-		width: '100%',
-		marginTop: theme.spacing(3),
-	},
-	table: {
-		minWidth: 500,
-	},
-	tableWrapper: {
-		overflowX: 'auto',
-	},
-}));
-
-export default function CustomPaginationActionsTable() {
-  const classes = useStyles2();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-  function handleChangePage(event, newPage) {
-    setPage(newPage);
-  }
-
-  function handleChangeRowsPerPage(event) {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  }
-
-  return (
-    <Paper className={classes.root}>
-      <div className={classes.tableWrapper}>
-        <Table className={classes.table}>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-              </TableRow>
-            ))}
-
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 48 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                colSpan={3}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { 'aria-label': 'rows per page' },
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
-    </Paper>
-  );
-}
+export default Organizers;
