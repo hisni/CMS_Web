43f3aa798dae5e03ea6ebc9fe87873933b307922
Hisni <hisnionline@gmail.com>
2020-08-25 00:27:13 +0530

    Updates

--- src/containers/Functions/User.js
+++ src/containers/Functions/User.js
@@ -1,35 +1,161 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-// import { Redirect } from 'react-router-dom';
+import UserLayout from '../Profile/UserLayout';
+import { Redirect } from 'react-router-dom';
 
 import './User.css';
-// import Spinner from '../../../components/UI/Spinner/Spinner';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
+import Input from '../../components/UI/Input/Input';
+import { updateObject, checkValidity } from '../../shared/utility';
+import Spinner from '../../components/UI/Spinner/Spinner'
+
 
 class User extends Component {
     state = {
-        loadedPost: null,
-        delete: false,
-        deleteSuccess:false
+        Form: {
+            NewRole: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'Author', displayValue: 'Author'},
+                        {value: 'Reviewer', displayValue: 'Reviewer'},
+                        {value: 'Admin', displayValue: 'Admin'},
+                    ]
+                },
+                value: '',
+                validation: {},
+                valid: true
+            },
+        },
+        Data:null,
+        submitted: false,
+        formIsValid: false,
     }
 
     componentDidMount() {
-        if ( this.props.match.params.id ) {
+        if ( this.props.match.params.uid ) {
             console.log(this.props.match.params.uid);
-            axios.get( "https://ecsuop2020.firebaseio.com/Speakers/" + this.props.match.params.id + '.json' )
-                .then( response => {
-                    this.setState( { loadedPost: response.data } );
-                } );
+            let token=  "Bearer " + this.props.token;
+            
+            let url = "userdata/users/" + this.props.match.params.uid;
+            // console.log(url)
+            axios.get(url, {headers: {Authorization: token}} )
+            .then( response => {                
+                this.setState({Data:response.data.result[0]})
+                console.log(this.state.Data)
+            }).catch(err => {
+                console.log(err);
+            });
         }
     }
 
+    inputChangedHandler = (event, PostIdentifier) =>{
+        const updatedPostForm = updateObject( this.state.Form, {
+            [PostIdentifier]: updateObject( this.state.Form[PostIdentifier], {
+                value: event.target.value,
+                valid: checkValidity( event.target.value, this.state.Form[PostIdentifier].validation ),
+                touched: true
+            } )
+        } );
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedPostForm) {
+            formIsValid = updatedPostForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({Form: updatedPostForm, formIsValid: formIsValid});
+
+    }
+
+    saveDataHandler = (event) => {
+
+        event.preventDefault();
+        
+
+        const data = {
+            userId: this.state.Data.id,
+            role : this.state.Form.NewRole.value,
+        };
+
+        console.log(data)
+        
+        let token=  "Bearer " + this.props.token;
+            
+        let url = "userdata/roles";
+        
+        axios.put(url, data, {headers: {Authorization: token}})
+            .then( response => {                
+                console.log(response)
+                this.setState({submitted:true});
+            }).catch(err => {
+                console.log(err);
+            });
+
+    }
+
     render () {
+
+        let redirect = null;
+        
+        if( this.state.submitted ){
+            redirect = <Redirect to={"/dashboard"}/>
+        }
+
+        let details = <Spinner />;
+        if( this.state.Data ){
+            let data = this.state.Data;
+            details = (
+                <Aux>
+                    <div  className="Name">
+                        <h1>{data.first_name + " " +data.last_name }</h1>
+                        <h1>Email: {data.email}</h1>
+                        <h1>Country Code: {data.country_ode}</h1>
+                        <h1>Current Role: {data.user_role}</h1>
+                    </div>
+                </Aux>
+            )
+        }
+
+        const formElementsArray = [];
+        for (let key in this.state.Form) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.Form[key]
+            });
+        }
+        let form = (
+            formElementsArray.map(formElement => (
+                <Input 
+                    key={formElement.id}
+                    label={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
+                    touched={formElement.config.touched}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            ))
+        );
         
         return(
             <Aux>
-                <div className="Page">
-                </div>
+                <UserLayout>
+                    <div className="Title">
+                        <h1>Users</h1>                    
+                    </div>
+                    <div>
+                        {redirect}
+                        {details}
+                    </div>
+                    <div className="RoleStyle">
+                        <form onSubmit={this.saveDataHandler} >
+                            {form}
+                            <button className="CB" disabled={!this.state.formIsValid} >Change</button>
+                        </form>
+                    </div>
+                </UserLayout>
             </Aux>
         );
     }
@@ -38,7 +164,7 @@ class User extends Component {
 const mapStateToProps = state => {
     return {
         isAuthenticated: state.auth.token !== null,
-        id: state.auth.token,
+        token: state.auth.token,
     }
 }
 
--- src/containers/Functions/Users.js
+++ src/containers/Functions/Users.js
@@ -121,11 +121,10 @@ class Profile extends Component {
             for(let key in response.data.result){
                 fetchedPosts.push({
                     ...response.data.result[key],
-                    id: key
                 });
             }
             this.setState({Data: fetchedPosts});
-            console.log(this.state.Data);
+            // console.log(response.data.result);
         } );
     }
 
@@ -147,14 +146,18 @@ class Profile extends Component {
 		var rows = [];
         if( this.state.Data ){
             this.state.Data.map(post => {
-                rows.push({
-                    ID: post.id,
-                    FN: post.first_name,
-                    LN: post.last_name,
-                    EM: post.email,
-					CC: post.country_code,
-					UR: post.user_role
-                });
+
+				if( post.user_role !== "SuperAdmin"){
+					rows.push({
+						ID: post.id,
+						FN: post.first_name,
+						LN: post.last_name,
+						EM: post.email,
+						CC: post.country_code,
+						UR: post.user_role
+					});
+				}
+                
                 return null;
             });
 		}
@@ -164,8 +167,6 @@ class Profile extends Component {
 		const emptyRows = this.state.rowsPerPage - Math.min(this.state.rowsPerPage, rows.length - this.state.page * this.state.rowsPerPage);
 
 		const classes = this.props;
-
-		console.log(this.props);
 
 		return (
 			<UserLayout>
@@ -241,65 +242,3 @@ const mapStateToProps = state => {
 }
 
 export default withStyles(styles)(connect(mapStateToProps)(Profile));
-// export default connect(mapStateToProps)(Profile);
-
-// var data = [];
-    
-//         if( this.state.Data ){
-//             this.state.Data.map(post => {
-//                 data.push({
-//                     ID: post.id,
-//                     FN: post.first_name,
-//                     LN: post.last_name,
-//                     EM: post.email,
-//                     CC: post.country_code
-//                     // Humidity: post.Humidity,
-//                     // stab: post.Stability
-//                 });
-//                 return null;
-//             });
-//         }
-
-//         data = data.slice(-25);
-
-//         console.log(data);
-
-//         var tableData = (
-//             data.map(TD => (
-//                 <tr>
-//                     <td>{TD.ID}</td>
-//                     <td>{TD.FN}</td>
-//                     <td>{TD.LN}</td>
-//                     <td>{TD.EM}</td>
-//                     <td>{TD.CC}</td>
-//                 </tr>
-//             ))
-//         );
-
-//         return(
-//             <AUX>
-//                 <UserLayout>
-//                     <div className="TrackBg" >
-//                         <div className="Title">
-//                             <h1>Users</h1>                    
-//                         </div>
-//                         <div className="Main">
-//                             <div className="Search">
-//                                 <label for="sc">Search: </label>
-//                                 <input type="search" id="gsearch" name="dc"/> 
-//                             </div>
-//                             <table id="Tdata">
-//                                 <tr>
-//                                     <th>ID</th>
-//                                     <th>First Name</th>
-//                                     <th>Last Name</th>
-//                                     <th>Email</th>
-//                                     <th>Country Code</th>
-//                                 </tr>
-//                                 {tableData}
-//                             </table>
-//                         </div>
-//                     </div>
-//                 </UserLayout>
-//             </AUX>
-//         );
